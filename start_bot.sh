0841908b8e16e9609d9e6674f9e366330235d113
jackjakarta <al.termure@gmail.com>
2023-11-12 01:57:31 +0100

    shell script update

--- start_bot.sh
+++ start_bot.sh
@@ -9,5 +9,3 @@ nohup python3 -u main.py &>> activity.log &
 sleep 2
 
 echo "Bot started in the background. Check 'activity.log' file for console output!"
-
-curl -d "ElonGPT Bot is up and running!" 139-162-166-208.ip.linodeusercontent.com/coffee
