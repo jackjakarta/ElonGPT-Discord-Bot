cc59067326cfe34affe2b8d0426df531013bba5a
jackjakarta <al.termure@gmail.com>
2024-03-05 02:01:29 +0100

    refactoring and removing repeteaing code

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,9 @@
 import json
+import random
+import string
+import time
+from datetime import datetime, timezone, timedelta
+
 from discord import Embed
 
 
@@ -25,3 +30,45 @@ def save_json(filename, data):
 def create_embed(title, description, color=0x4BA081):
     """Create a Discord embed."""
     return Embed(title=title, description=description, color=color)
+
+
+class RandomGenerator:
+    """Random String Generator. Default length is 10 characters."""
+    def __init__(self):
+        self.length = None
+
+    def random_string(self, length=6):
+        self.length = length
+
+        characters = string.ascii_lowercase + string.digits
+        random_generate = "".join(random.choices(characters, k=self.length))
+
+        return random_generate
+
+    def random_digits(self, length=6):
+        self.length = length
+
+        characters = string.digits
+        random_generate = "".join(random.choices(characters, k=self.length))
+
+        return random_generate
+
+    def random_letters(self, length=6):
+        self.length = length
+
+        characters = string.ascii_letters
+        random_generate = "".join(random.choices(characters, k=self.length))
+
+        return random_generate
+
+    @staticmethod
+    def timestamp(tz_delta: int = 1):
+        timestamp = time.time()
+        time_zone = timezone(timedelta(tz_delta))
+        datetime_obj = datetime.fromtimestamp(timestamp, tz=time_zone)
+        formatted_date = datetime_obj.strftime('%d-%m-%Y')
+        formatted_time = datetime_obj.strftime('%H-%M')
+
+        timestamp_formatted = f"{formatted_date}_{formatted_time}"
+
+        return timestamp_formatted
