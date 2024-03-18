5791647a50adecda3839a731a37c4e05ee7e279f
jackjakarta <al.termure@gmail.com>
2024-03-18 16:26:55 +0100

    changed embed color to white

--- utils/__init__.py
+++ utils/__init__.py
@@ -46,7 +46,7 @@ def save_json(filename, data):
         json.dump(data, file, indent=4)
 
 
-def create_embed(title, description, color=0x4BA081):
+def create_embed(title, description, color=0xFFFFFF):
     """Create a Discord embed."""
     return Embed(title=title, description=description, color=color)
 
